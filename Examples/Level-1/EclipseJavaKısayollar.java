9e87b4cbabf4719075dca54cfa55653e6ec94a25
OguzHan24 <oguzhan1724@hotmail.com>
2014-10-07 20:16:17 +0300

    Added Eclipse_Kısayollar

---
+++ Examples/Level-1/EclipseJavaKısayollar.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class EclipseJavaKısayollar {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+	}
+
+	/*
+	 * CTRL + D Bulunduğunuz satırı siler.
+	 * CTRL + L Numarasını verebileceğiniz satıra atlar.
+	 * SHIFT + CTRL + UP(YUKARI YÖN TUŞU & AŞAĞI YÖN TUŞU )
+	 * Methodlar arasında hızlı geçiş yapmanızı sağlar. 
+	 * CTRL + M Bulunduğunuz
+	 * alanı genişletir, genişletilmiş halde ise küçültür.
+	 * ALT + UP OR DOWN
+	 * kısayolu ile bulunduğunuz satırdaki kodu yukarı aşağı taşıyabiliriz.
+	 * SHIFT + ALT + J
+	 *  Sınıf / Method başlarına Javadoc yorumaları ekler.
+	 *  SHIFT + CTRL + C
+	 *  Seçili satırı yorum satırı haline getirir.
+	 *  SHIFT + CTRL + O Import’ları
+	 * düzenler. Gereksiz import’ları kaldırır. Eklenmesi gereken import’ları
+	 * ekler.Bazen kullandığınız bir sınıf adı birden fazla paketin altında
+	 * bulunabilir.Böyle bir durumda SHIFT + CTRL + O dediğinizde size
+	 * hangisini* ekleyeceğinizi de sorar.
+	 * SHIFT + CTRL + F Kodun girintilerini
+	 * stilini düzeltir.Girinti şu kadar olsun, noktadan sonra boşluk olsun gibi
+	 * ayarları Preferences’ten yaparız.
+	 * SHIFT+ CTRL + B İmlecin olduğu satıra BreakPoint koyar. Koyukmuş bir breakPointi kaldırır.
+	 * F 11 Debug ı başlatır.
+	 * F5 (Step into)
+		Debug yaparken bir metod tanımında iseniz işletime o çağrının içinden devam eder 
+		ve bazı durumlarda metodun içinde neler olup bittiğine bakmanız gerekiyorsa 
+		çok yararlı olur
+	 * CTRL + BACKSPACE İle bulunduğumuz satırdaki yazılanı siler , satırı komple silmez.
+	 * 
+	 */
+
+	
+}
