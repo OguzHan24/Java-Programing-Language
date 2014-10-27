f3852657f2f9c64ec04043e150bce0637ecfedf9
OguzHan24 <oguzhan1724@hotmail.com>
2014-10-27 15:37:02 +0200

    first commit

---
+++ Examples/Level-3/PolymorphismExample/GeometricalFigure.java
@@ -0,0 +1,17 @@
+package Polymorphism_CokBicimlilik;
+
+public class GeometricalFigure 
+{
+	public GeometricalFigure()
+	{
+	/*Default Cons*/	
+	}
+	
+	public double areaCalculation()
+	{
+		return 0;
+	}
+	
+	
+
+}
