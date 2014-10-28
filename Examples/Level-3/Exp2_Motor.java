dfa0dfb5ba32b00ca4d6b61ad44816f0b5581b53
OguzHan24 <oguzhan1724@hotmail.com>
2014-10-28 23:08:37 +0200

    first commit

---
+++ Examples/Level-3/Exp2_Motor.java
@@ -0,0 +1,21 @@
+package Kalıtım_Inheritance;
+
+public class Exp2_Motor 
+{
+	// Java da kompozisyon örneği 
+	
+	private int güc = 130;
+	
+	public void calistir()
+	{
+		System.out.println("Motor çalıştırıldı ! ");
+	}
+	public void motorDur()
+	{
+		System.out.println("Motor stop! ");
+	}
+	
+	 
+	 
+	
+}
