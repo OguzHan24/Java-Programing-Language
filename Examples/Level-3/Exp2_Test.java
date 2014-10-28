7661b591a052b2affc01d5a497ec2da13a2eeaf0
OguzHan24 <oguzhan1724@hotmail.com>
2014-10-28 23:09:21 +0200

    first commit

---
+++ Examples/Level-3/Exp2_Test.java
@@ -0,0 +1,14 @@
+package Kalıtım_Inheritance;
+
+public class Exp2_Test 
+{
+	
+	public static void main(String[] args)
+	{
+		Exp2_Araba arabaN = new Exp2_Araba();
+		
+		arabaN.hareketEt();
+		arabaN.durArtık();
+	}
+
+}
