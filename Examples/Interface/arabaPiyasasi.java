ea2a73052082a3d806b1bea082f1feba16608eef
OguzHan24 <oguzhan1724@hotmail.com>
2015-02-19 01:23:40 +0200

    First Commit

---
+++ Examples/Interface/arabaPiyasasi.java
@@ -0,0 +1,17 @@
+package arabalarr;
+
+public  class arabaPiyasasi implements Arabalar 
+{
+	public int arabaninPiyasasi()
+	{
+		return 0;
+	}
+
+	@Override
+	public int arabaninOzellikleri() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
