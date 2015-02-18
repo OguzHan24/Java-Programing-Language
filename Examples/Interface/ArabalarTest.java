66fed06440a2447a80c50bcb60f7096f63bbcb0f
OguzHan24 <oguzhan1724@hotmail.com>
2015-02-19 01:21:37 +0200

    First commit

---
+++ Examples/Interface/ArabalarTest.java
@@ -0,0 +1,17 @@
+package arabalarr;
+
+public class ArabalarTest {
+
+	/**
+	 * @author oguzhan ural
+	 */
+	public static void main(String[] args) {
+
+		Arabalar arabaNesne = new arabaOzellikleri();
+		arabaNesne.arabaninOzellikleri();
+		// Burada araba nesnemiz ile arabanin özellikleri metodunu çağırıyoruz.
+		// implementasyon yaptığımız arabaOzellikleri() metodunu kullanarak nesne oluşturduk ve 
+		// bu nesne ile de metodu çağırdık.
+	}
+
+}
